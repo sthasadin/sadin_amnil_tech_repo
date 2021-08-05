2aba8cc7a13db0aec5405aaacf0dc9deace796ce
sthasadin <shrestha.sadin007@gmail.com>
2021-08-05 09:20:53 +0545

    addin css file

--- js/factorial.js
+++ js/factorial.js
@@ -1,6 +1,8 @@
+console.log('Factorial');
 var num = 6;
 
     var result = num;
+    function factorial(){
     if (num === 0 || num === 1) {
     console.log('1');
     }
@@ -13,4 +15,4 @@ var num = 6;
     }
 }
     
-  
+};
--- js/fibonachi.js
+++ js/fibonachi.js
@@ -3,10 +3,13 @@ var number = 6;
 let a1 = 0, a2 = 1, temp;
 
 console.log('Fibonacci Series:');
-
+function fibo(){
 for (let i = 1; i <= number; i++) {
+    
     console.log(a1);
     temp = a1 + a2;
     a1 = a2;
     a2 = temp;
 }
+};
+fibo();
